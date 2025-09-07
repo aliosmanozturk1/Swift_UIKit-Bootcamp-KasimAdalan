0f4940e095984f6320601048fb29a27fde77f29c
Ali Osman Öztürk <github@aliosmanozturk.me>
2025-09-07 21:22:15 +0300

    Create Blank Project for Odev04 Programmatic

---
+++ Odev04_Navigation_Programmatic/Odev04_Navigation_Programmatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Odev04_Navigation_Programmatic
+//
+//  Created by Ali Osman Öztürk on 7.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
